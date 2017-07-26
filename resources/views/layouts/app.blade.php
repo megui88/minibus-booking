e551b052bf8d5d4b351b7e427fd4b822e5915b05
Mariano G. Egui <egui.mariano@gmail.com>
2017-07-26 14:39:28 -0300

    welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,80 +14,87 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ route('dashboard') }}">
-                        {{ config('app.name', 'Dashboard') }}
-                    </a>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ route('dashboard') }}">
+                    {{ config('app.name', 'Dashboard') }}
+                </a>
+            </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+
+            @if (!Auth::guest())
+                <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="{{ route('incompletes') }}">Servicios incompletos</a></li>
                         <li><a href="{{ route('prices') }}">Reglas de Agencias</a></li>
                         <li><a href="{{ route('liquidated') }}">Liquidaciones</a></li>
                     </ul>
+            @endif
+            <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li>
+                            <a>
+                                <button type="button" autocomplete="off"
+                                        class="btn btn-sm"
+                                        v-bind:class="{ 'btn-primary': cache, 'btn-warning': !cache  }"
+                                        v-bind:class="{ active: cache }"
+                                        @click="cache = !cache"
+                                >CACHE
+                                </button>
+                            </a>
+                        </li>
+                        <li><a href="{{ route('settings') }}">Configuraciones</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li>
-                                <a><button type="button" autocomplete="off"
-                                           class="btn btn-sm"
-                                           v-bind:class="{ 'btn-primary': cache, 'btn-warning': !cache  }"
-                                           v-bind:class="{ active: cache }"
-                                           @click="cache = !cache"
-                                    >CACHE</button>
-                                </a>
-                            </li>
-                            <li><a href="{{ route('settings') }}">Configuraciones</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                                        Logout
+                                    </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        @yield('content')
-    </div>
+    @yield('content')
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('javascripts')
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('javascripts')
 </body>
 </html>
